e28765fdd261d2b9d9c5a038907d17296d25ae99
TRX Sol <rohitkaushik2001@gmail.com>
2026-09-14 22:44:29 +0530

    Update CustomPython3Recipe to disable grp module
    Remove 'disable-grp.patch' and set environment variable to disable 'grp' module.

--- p4a-recipes/python3/__init__.py
+++ p4a-recipes/python3/__init__.py
@@ -4,9 +4,11 @@ from pythonforandroid.recipes.python3 import Python3Recipe
 class CustomPython3Recipe(Python3Recipe):
     name = "python3"
     version = "3.12.14"
-    patches = [
-        "disable-grp.patch",
-    ]
+
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        env["PYTHON_DISABLE_MODULES"] = "grp"
+        return env
 
 
 recipe = CustomPython3Recipe()
